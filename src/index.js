070653adb46d623fdae9b97f601d78e9c1fa6fb7
Alexandra <75269203+alexandralit@users.noreply.github.com>
2022-07-22 10:03:50 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './assets/styles/index.scss';
 
 document.addEventListener("DOMContentLoaded", function () {
 
-  const valuePower = this.querySelector(".form-label");
-  const range = this.querySelector(".form-range");
+  const valuePower = document.querySelector(".form-label");
+  const range = document.querySelector(".form-range");
 
   range.addEventListener("input", () => {
     valuePower.innerHTML = `${range.value} hp`;
@@ -11,11 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const menu = document.querySelector(".menu");
   const burger = document.querySelector(".menu-icon");
-  const closeMenu = document.querySelector(".bi-x-lg");
+  const crossMenu = document.querySelector(".bi-x-lg");
 
   const toggleMenu = () => menu.classList.toggle("active");
 
   burger.addEventListener("click", toggleMenu);
-
-  closeMenu.addEventListener("click", toggleMenu);
+  crossMenu.addEventListener("click", toggleMenu);
 });
